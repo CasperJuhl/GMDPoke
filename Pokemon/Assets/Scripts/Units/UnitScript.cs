e99ba76f8e637699731441002a9d0fdad7dc3572
CasperJuhl <Casperbjuhl@gmail.com>
2021-04-06 15:16:22 +0200

    Added player and enemy battle

---
+++ Pokemon/Assets/Scripts/Units/UnitScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitScript : MonoBehaviour
+{
+    public string Name;
+    public int Level;
+
+    public double damage;
+
+    public int HP;
+    public int currentHP;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
