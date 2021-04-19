f9d848315b7b5158e061ce9611b3aec86ee4b97e
CasperJuhl <Casperbjuhl@gmail.com>
2021-04-19 14:47:20 +0200

    Added animations for fading in/out

--- Pokemon/Assets/Scripts/Player/PlayerController.cs
+++ Pokemon/Assets/Scripts/Player/PlayerController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
@@ -75,7 +76,7 @@ public class PlayerController : MonoBehaviour
         {
             if (Random.Range(1, 101) <= 10)
             {
-                Debug.Log("ENCOUNTER!");
+                SceneManager.LoadScene("BattleScene");
             }
         }
     }
